061d9c245a379c870869b80c2a258766bd518697
Mykhailo Yazenok <mikle3ddd@gmail.com>
2023-05-15 18:33:22 +0300

    fix: specify libraries' versions

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ setup(name='word2vecext',
       author='Mihail Yazenok',
       author_email='mihailyazenok@gmail.com',
       url='https://github.com/MihailYa/word2vec-ext',
-      version='0.0.1',
+      version='0.0.2',
       py_modules=['word2vec_ext.word2vec', 'word2vec_ext.ecg_processing', 'word2vec_ext.kmeans'],
       install_requires=[
-          "wfdb",
+          "wfdb=3.4.1",
           "sklearn",
-          "gensim",
-          "pandas",
-          "scikit-learn",
-          "numpy",
-          "matplotlib",
-          "py-ecg-detectors"
+          "gensim=4.1.2",
+          "pandas=1.1.5",
+          "scikit-learn=1.0.1",
+          "numpy=1.21.5",
+          "matplotlib=3.5.1",
+          "py-ecg-detectors=1.1.0"
       ]
       )
